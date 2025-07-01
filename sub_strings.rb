0f1975e1e514f66aa0d30c7fb5c423d2f99a73a7
jbrenesp <josembrenes@gmail.com>
2025-07-01 14:12:47 +0300

    String new hash and dictionary added to substrings

---
+++ sub_strings.rb
@@ -0,0 +1,14 @@
+def substrings(string, dictionary)
+  string = string.downcase
+  hash = {}
+  dictionary.each do |word|
+    
+end
+
+  > dictionary = ["below","down","go","going","horn","how",
+  "howdy","it","i","low","own","part","partner","sit"]
+  => ["below","down","go","going","horn",
+  "how","howdy","it","i","low","own","part","partner","sit"]
+
+  puts substrings("below", dictionary)
+
