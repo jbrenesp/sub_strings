1165bf6066c0b7cbad9f359570f7ca485c73ed8e
jbrenesp <josembrenes@gmail.com>
2025-07-02 15:40:41 +0300

    Substrings ruby project completed

--- sub_strings.rb
+++ sub_strings.rb
@@ -2,12 +2,15 @@ def substrings(string, dictionary)
   string = string.downcase
   hash = {}
   dictionary.each do |word|
-    
+    count = string.scan(word).length
+    hash[word] = count if count > 0
+  end
+  hash
 end
 
-  > dictionary = ["below","down","go","going","horn","how",
+  dictionary = ["below","down","go","going","horn","how",
   "howdy","it","i","low","own","part","partner","sit"]
-  => ["below","down","go","going","horn",
+  ["below","down","go","going","horn",
   "how","howdy","it","i","low","own","part","partner","sit"]
 
   puts substrings("below", dictionary)
